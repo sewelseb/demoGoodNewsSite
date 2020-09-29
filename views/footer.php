2252f9e9df00e6e6b16c344e0457d24828814666
sewelseb <sewelseb@hotmail.com>
2020-09-29 17:17:23 +0200

    add - 2

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,5 @@
 		<div id="pied">
-			Sébastien de Beauffort
+			Sébastien de Beauffort 2
 			<strong>Excellente journée qu'aujourd'hui le <?php echo $date ?></strong> :: 
 			<?php $time_end = microtime(true);
 				  $time = number_format(($time_end - $time_start)*1000,6);
