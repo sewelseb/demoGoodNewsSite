4e7f506a4ca374dc92dc686a1f365eb34a62a96d
sewelseb <sewelseb@hotmail.com>
2020-10-06 16:10:08 +0200

    change  - home

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 <div id="contenu">
-	<h2>Accueil</h2>
+	<h2>Homme</h2>
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 </div><!-- #contenu -->
